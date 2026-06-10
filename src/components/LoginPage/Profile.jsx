b531361e323eb4d0c5bc39c9fd6026e73a416ce8
Archit Nigam <architnigam72@gmail.com>
2026-06-10 13:30:46 +0530

    more

--- src/components/LoginPage/Profile.jsx
+++ src/components/LoginPage/Profile.jsx
@@ -27,8 +27,7 @@ function Profile() {
     <div className="profile-container">
         <div className="profile-content">
             <h2>Welcome back, {username}!</h2>
-            <p>This is your profile page. Here you can view and manage your account details, update your preferences, and access personalized features.</p>
-            <p>Feel free to explore the various sections of your profile and make any necessary updates to ensure your information is accurate and up-to-date.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </div>
     </div>
     );
